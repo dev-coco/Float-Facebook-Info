5211d59e528cad723f1d3a45ae89bd2331400a6a
dev-coco <63853884+dev-coco@users.noreply.github.com>
2021-02-11 20:07:28 -0400

    Add files via upload

--- inject.js
+++ inject.js
@@ -1,8 +1,6 @@
 var check_url = location.href;
-var reg = RegExp(/groups|watchparty|\/wp\/|marketplace|watch|search/);
-if (check_url.match(reg)) {
-    console.log("url_include");
-} else {
+var reg = RegExp(/groups|watchparty|\/wp\/|marketplace|watch|search|pages|community|live_videos/);
+if (check_url.match(reg)) {} else {
     try {
         var para = document.createElement("p");
         var node = document.createTextNode("");
@@ -14,75 +12,94 @@ if (check_url.match(reg)) {
         para.setAttribute("ondblclick", "document.getElementById('facebook_info').style.display='none'");
     } catch {}
     chrome.storage.local.get({
-        token: ''
-    }, ({
-        token
-    }) => {
-        (async function() {
-            var get_html = document.documentElement.outerHTML;
-            try {
-                var facebookID = get_html.match(/(?<=userID":")[0-9].*?[0-9](?=")/g)[0];
-                let response = await fetch('https://graph.facebook.com/' + facebookID + '?fields=location,hometown,name,languages,religion,education,birthday&access_token=' + token + '&pretty=0')
-                let text1 = await response.text()
-                try {
+            token: ''
+        }, ({
+            token
+        }) => {
+            (async function() {
+                    var get_html = document.documentElement.outerHTML;
                     try {
-                        var city = text1.match(/(?<=location":{"id":".*?","name":").*?(?="},"hometown)/g)[0];
-                    } catch {
+                        var facebookID = get_html.match(/(?<=userID":")[0-9].*?[0-9](?=")/g)[0];
+                        let response = await fetch('https://graph.facebook.com/' + facebookID + '?fields=location,hometown,name,languages,religion,education,birthday,gender,updated_time&access_token=' + token + '&pretty=0')
+                        let text1 = await response.text()
                         try {
-                            var city = text1.match(/(?<=location":{"id":".*?","name":").*?(?="},"name)/g)[0];
-                        } catch {
-                            var city = "未知";
-                        }
-                    }
-                    var city_str = eval("`" + city + "`");
-                    try {
-                        var hometown = text1.match(/(?<=hometown":{"id":".*?","name":").*?(?="},"name)/g)[0];
-                    } catch {
-                        var hometown = "未知";
-                    }
-                    var hometown_str = eval("`" + hometown + "`");
-                    try {
-                        var language = text1.match(/(?<=languages":\[).*?(?=])/g)[0].replace(/id":".*?"|,|name|"|:/g, "");
-                    } catch {
-                        var language = "未知";
-                    }
-                    var language_str = eval("`" + language + "`");
-                    try {
-                        var religion = text1.match(/(?<=religion":").*?(?=",)/g)[0];
-                    } catch {
-                        var religion = "未知";
-                    }
-                    var religion_str = eval("`" + religion + "`");
+                            try {
+                                var city = text1.match(/(?<=location":{"id":".*?","name":").*?(?="},"hometown)/g)[0];
+                            } catch {
+                                try {
+                                    var city = text1.match(/(?<=location":{"id":".*?","name":").*?(?="},"name)/g)[0];
+                                } catch {
+                                    var city = "\u672A\u77E5";
+                                }
+                            }
+                            var city_str = eval("`" + city + "`");
+                            try {
+                                var hometown = text1.match(/(?<=hometown":{"id":".*?","name":").*?(?="},"name)/g)[0];
+                            } catch {
+                                var hometown = "\u672A\u77E5";
+                            }
+                            var hometown_str = eval("`" + hometown + "`");
+                            try {
+                                var language = text1.match(/(?<=languages":\[).*?(?=])/g)[0].replace(/id":".*?"|,|name|"|:/g, "");
+                            } catch {
+                                var language = "\u672A\u77E5";
+                            }
+                            var language_str = eval("`" + language + "`");
+                            try {
+                                var religion = text1.match(/(?<=religion":").*?(?=",)/g)[0];
+                            } catch {
+                                var religion = "\u672A\u77E5";
+                            }
+                            var religion_str = eval("`" + religion + "`");
 
-                    try {
-                        var college = text1.match(/(?<=education":\[).+(?="type":"College")/g)[0].replace(/^{|id":".*?"|,|name|:|"school"|"year"|,|"|type":".*?"|concentration":\[.*?\]/g, "").replace(/}{|{[0-9].*?[0-9]}|classes\[.*?\]|}\]|degree/g, "");
-                    } catch {
-                        var college = "未知";
-                    }
-                    var college_str = eval("`" + college + "`");
+                            try {
+                                var college = text1.match(/(?<=education":\[).+(?="type":"College")/g)[0].replace(/^{|id":".*?"|,|name|:|"school"|"year"|,|"|type":".*?"|concentration":\[.*?\]/g, "").replace(/}{|{[0-9].*?[0-9]}|classes\[.*?\]|}\]|degree/g, "");
+                            } catch {
+                                var college = "\u672A\u77E5";
+                            }
+                            var college_str = eval("`" + college + "`");
 
-                    try {
-                        var birthday = text1.match(/(?<=birthday":").*?(?=","id)/g, "")[0].replace(/\\/g, "");
-                    } catch {
-                        var birthday = "未知";
-                    }
-                    // This is just a demo, if you want to use it, you need to customize it.
-                    /*
-                    var bad_people_id = RegExp(//g);
-                    try {
-                        if (facebookID.match(bad_people_id)) {
-                            var bad_people = "<br><span style=\"font-size:25px;\">❌</span>";
-                        } else {
-                            var bad_people = "";
-                        }
-                    } catch {
-                        var bad_people = "";
-                    }
-                    */
-                } catch {}
-                var str = `<div style="text-align:center;">⚠️请勿截图</div>所在地：` + city_str + `<br>家乡：` + hometown_str + `<br>信仰：` + religion_str + `<br>语言：` + language_str + `<br>大学：` + college_str + `<br>生日：` + birthday /* + bad_people */;
-                document.getElementById("facebook_info").innerHTML = str;
-            } catch {}
-        })();
-    })
-}
+                            try {
+                                var birthday = text1.match(/(?<=birthday":").*?(?=")/g)[0].replace(/\\/g, "");
+                            } catch {
+                                var birthday = "\u672A\u77E5";
+                            }
+
+                            try {
+                                var update_time = text1.match(/(?<=updated_time":").*?(?=T)/g)[0];
+                            } catch {
+                                var update_time = "\u672A\u77E5";
+                            }
+                            try {
+                                var gender = text1.match(/(?<=gender":").*?(?=")/g)[0];
+                                if (gender == "female") {
+                                    var gender_str = "\u5973";
+                                } else if (gender == "male") {
+                                    var gender_str = "\u7537";
+                                } else {
+                                    var gender_str = "\u672A\u77E5";
+                                }
+                            } catch {
+                                var gender_str = "\u672A\u77E5";
+                            }
+                            // This is just an example, if you want to use it, you need to customize it.
+                            /*
+                            var bad_people_id = RegExp(//g)；
+                                try {
+                                    if (facebookID.match(bad_people_id)) {
+                                        var bad_people = "<br><span style=\"font-size:25px;\">❌</span>";
+                                    } else {
+                                        var bad_people = "";
+                                    }
+                                } catch {
+                                    var bad_people = "";
+                                }
+                             */
+                            }
+                            catch {}
+                            var str = `<div style="text-align:center;">⚠️\u8BF7\u52FF\u622A\u56FE</div>\u6240\u5728\u5730：` + city_str + `<br>\u5BB6\u4E61：` + hometown_str + `<br>\u4FE1\u4EF0：` + religion_str + `<br>\u8BED\u8A00：` + language_str + `<br>\u5927\u5B66：` + college_str + `<br>\u6027\u522B：` + gender_str + `<br>\u751F\u65E5：` + birthday + `<br>\u6D3B\u8DC3\u65F6\u95F4：` + update_time /* + bad_people */ ;
+                            document.getElementById("facebook_info").innerHTML = str;
+                        } catch {}
+                    })();
+            })
+    }
